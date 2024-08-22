36550eeb4c59a8c8a9822aeaba972d09921c02e4
Róbert Klimant <roboklimant@gmail.com>
2024-08-22 11:21:12 +0200

    fix: handling missing elements

--- src/utils/email-modal.ts
+++ src/utils/email-modal.ts
@@ -1,9 +1,18 @@
 export const emailModal = () => {
   const button = document.querySelectorAll('[data-button="career"]') as NodeListOf<HTMLElement>;
+  if (!button) return;
+
   const sendEmailButton = document.querySelector('[data-button="apply"]') as HTMLElement;
+  if (!sendEmailButton) return;
+
   const childEmailButton = sendEmailButton.querySelector('a') as HTMLElement;
+  if (!childEmailButton) return;
+
   const defaultEmail = childEmailButton.getAttribute('href');
+  if (!defaultEmail) return;
+
   const titleModal = document.querySelector('[title-modal]') as HTMLElement;
+  if (!titleModal) return;
 
   button.forEach((elBut) => {
     elBut.addEventListener('click', () => {
